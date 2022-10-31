1437d9ada1f37180f4ebbece86584817838a8af9
Bernardo <gurmock@gmail.com>
2022-10-31 06:59:47 +0100

    icons

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { FaQuoteLeft } from "react-icons/fa"
 
 function QuoteBox (){
-  return <div>
-    
-  </div>
+  return <div id="quote-box" className='quote-box-container'>
+            <FaQuoteLeft />
+        </div>
 };
 
 
